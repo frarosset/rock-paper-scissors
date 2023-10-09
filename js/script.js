778e07eaf5379894f0be5151e21cd530a4d9feca
frarosset <37275581+frarosset@users.noreply.github.com>
2023-10-09 18:28:34 +0200

    Create a JS function to get player choice through a prompt

--- js/script.js
+++ js/script.js
@@ -6,7 +6,44 @@
 // is located. 
 // This assumes that i+1 wins i (and 0 wins choices.length-1) 
 // (see function roundOutcome)
-let choices = ['Rock','Paper','Scissors']
+// Global variables:
+let choices = ['Rock','Paper','Scissors'];
+let choicesLowerCase = choices.map(itm => itm.toLowerCase());
+let uniqueInitials = choicesLowerCase.map(itm => itm[0]).filter((val, idx, arr)=>
+  arr.indexOf(val) === idx).length === choicesLowerCase.length;
+
+
+// Get user choice for the user (player)
+// A prompt is used
+// The user can type the full name of the choice or just the first letter (if these are unique)
+// The choice is case-insensitive
+// If the input field is left empty (undefined value), a random value is generated
+// Output: the ID of the choice (-1 if the choice is invalid)
+function getPlayerChoice(){
+	let descrStr1 = 'Make your choice: ' + choices + '\n';
+	let descrStr2 = '- The typed string is case-insensitive;\n'
+	let descrStr3 = uniqueInitials?'- You can optionally type only the first letter;\n':'';
+	let descrStr4 = '- If you leave the field empty, a random choice is generated.'
+
+	let userChoiceStr = prompt(descrStr1+descrStr2+descrStr3+descrStr4);
+	if (userChoiceStr=='' || userChoiceStr==undefined)
+		return getComputerChoice();
+
+	// trim initial and final white spaces, and convert to lowercase
+	userChoiceStr = userChoiceStr.trim().toLowerCase();
+	let checkInitial = uniqueInitials && userChoiceStr.length==1;
+
+	for (let i=0;i<choices.length; i++)
+	{
+		if (userChoiceStr === choicesLowerCase[i] ||
+		(checkInitial && (userChoiceStr===choicesLowerCase[i][0])))
+			return i;
+	}
+
+	/* Return -1 if no string match */
+	return -1;
+}
+
 
 // Create a random choice for the opponent (computer)
 // Output: the ID of the choice
@@ -54,3 +91,5 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
+// Get user choice
+// Output: user choice ID
