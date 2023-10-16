9143857708b084bc380966da2992e727fccd0018
frarosset <37275581+frarosset@users.noreply.github.com>
2023-10-16 18:40:32 +0200

    Add the game logic
    It is now possible to play the game with a graphical interface.
    
    DOM methods have been used to add events listerers to the buttons,
    and to update the displayed text.
    
    The style of the page has not been improved yet.

--- js/script.js
+++ js/script.js
@@ -5,47 +5,51 @@
 // (see function roundOutcome)
 const choices = ['Rock','Paper','Scissors'];
 const choicesLowerCase = choices.map(itm => itm.toLowerCase());
-const choicesLowerCaseInitials = choicesLowerCase.map(itm => itm[0]);
-const uniqueInitials = choicesLowerCaseInitials.filter((val,idx,arr)=>
-  arr.indexOf(val) === idx).length === choicesLowerCaseInitials.length;
-const userName = 'You';
+const playerName = 'You';
 const computerName = 'Computer';
-// These codes specify if a match is won or lost by the user, or if it is a tie
+let playerScore;
+let computerScore;
+// These codes specify if a match is won or lost by the player, or if it is a tie
 const winCode = 1;
 const loseCode = -1;
 const tieCode = 0;
+const scoreToWin = 5;
 
-// Get user choice ID for the user (player)
-// The user can type in a prompt the full name of the choice 
-// or just the first letter (if these are unique)
-// The choice is case-insensitive
-// If the input field is left empty ('' or undefined), a random ID is generated
-// Return -1 if the choice is invalid (no string match)
-function getPlayerChoice(){
-	let descrStr1 = 'Make your choice: ' + choices + '\n';
-	let descrStr2 = '- The typed string is case-insensitive;\n'
-	let descrStr3 = uniqueInitials?'- You can optionally type only the first letter;\n':'';
-	let descrStr4 = '- If you leave the field empty, a random choice is generated.';
+/* ************************************************************** */
 
-	let userChoiceStr = prompt(descrStr1+descrStr2+descrStr3+descrStr4);
+let gameInfoDiv = document.querySelector('#gameInfo');
+let playerChoiceInfoDiv = document.querySelector('#playerChoiceInfo');
+let roundOutcomeDiv = document.querySelector('#roundOutcome');
+let playersInfoDiv = document.querySelector('#playersInfo');
+let gameOutcomeDiv = document.querySelector('#gameOutcome');
+let playerMoveDiv = document.querySelector('#playerMove');
+let playerNameDiv = document.querySelector('#playerName');
+let playerScoreDiv = document.querySelector('#playerScore');
+let computerMoveDiv = document.querySelector('#computerMove');
+let computerNameDiv = document.querySelector('#computerName');
+let computerScoreDiv = document.querySelector('#computerScore');
+let playerChoiceListDiv = document.querySelector('#playerChoiceList');
+let newGameBtn = document.querySelectorAll('.newGameBtn');
+let newRoundBtn = document.querySelector('#newRoundBtn');
+let roundOutcomeP = document.querySelector('#roundOutcome>p');
+let gameOutcomeP = document.querySelector('#gameOutcome>p');
 
-	if (userChoiceStr=='' || userChoiceStr==undefined)
-		return getComputerChoice();
-
-	userChoiceStr = userChoiceStr.trim().toLowerCase();
-
-	let checkInitial = uniqueInitials && userChoiceStr.length==1;
-
-	for (let i=0;i<choices.length; i++)
-	{
-		if (userChoiceStr === choicesLowerCase[i] ||
-		(checkInitial && (userChoiceStr===choicesLowerCaseInitials[i])))
-			return i;
-	}
-
-	return -1;
+playerNameDiv.textContent = playerName;
+computerNameDiv.textContent = computerName;
+let playerChoiceBtn = [];
+for (let i=0;i<choices.length;i++){
+	playerChoiceBtn[i] = document.createElement('button');
+	playerChoiceBtn[i].type = 'button';
+	playerChoiceBtn[i].classList.add('choicesBtn',choicesLowerCase[i]);
+	playerChoiceBtn[i].playerChoice = i;
+	playerChoiceBtn[i].textContent = choices[i];
+	disableButton(playerChoiceBtn[i]);
+	playerChoiceListDiv.appendChild(playerChoiceBtn[i]);
 }
 
+/* ************************************************************** */
+
+/* Helper game functions ---------------------------------------- */
 
 // Create a random choice ID for the opponent (computer)
 function getComputerChoice(){
@@ -56,127 +60,98 @@ function getComputerChoice(){
 // This assumes that i+1 wins i (and 0 wins choices.length-1)
 function roundOutcome(playerSelection, computerSelection) {
   if (playerSelection===computerSelection)
-  	return tieCode;
+  	return [tieCode,`It's a tie!`]; 
   else if (((computerSelection+1)%choices.length) === playerSelection)
-  	// The user wins, as playerSelection>computerSelection
+  	// The player wins, as playerSelection>computerSelection
   	// Note that playerSelection+1===computerSelection is tested, instead.
   	// (computerSelection+1)%choices.length) is used to wrap around playerSelection+1
   	// when playerSelection = choices.length-1
-  	return winCode;
+  	return [winCode,`You Win! ${choices[playerSelection]} beats ${choices[computerSelection]}`];
   else
-  	return loseCode;
-  
+  	return [loseCode,`You Lose! ${choices[computerSelection]} beats ${choices[playerSelection]}`];
 }
 
-// Play a round, given the player and computer choices IDs, and return
-// its outcome as an array with a string description and a code
-function playRound(playerSelection, computerSelection) {
-  let outcome = roundOutcome(playerSelection, computerSelection);
-
-  switch(outcome){
-  	 case tieCode:
-  	 	return [`  It's a tie!`,outcome];
-  	 	break;
-  	 case winCode:
-	 	return [`  You Win! ${choices[playerSelection]} beats ${choices[computerSelection]}`,outcome];
-  	 	break;
-  	 case loseCode: 
-  	 	return [`  You Lose! ${choices[computerSelection]} beats ${choices[playerSelection]}`,outcome];
-  	 	break;
-  	 default: 	
-  	 	console.error(`Invalid outcome`);
-  }
-}
-
-function declareWinner(playerScore,computerScore){
-	if (playerScore>computerScore){
-		return `You won this game by ${playerScore} to ${computerScore}`;
-	} else if (playerScore<computerScore){
-		return `You lost this game by ${playerScore} to ${computerScore}`;
-	} else{
-		return `You neither won nor lost this game: it's a tie, ${playerScore} to ${computerScore}!`;
+function declareWinner(){
+	if (playerScore==scoreToWin){
+		return [winCode,`You won this game!`];
+	} else if (computerScore==scoreToWin){
+		return [loseCode,`You lost this game!`];
+	} else {
+		return [tieCode,``]; // no winner yet
 	}
-}
-
-// The main game function, where printFcn could be, e.g., console.log 
-// or alert and specifies where the messages are displayed
-function game(printFcn=console.log){
-	const numOfRounds = 5;
-	let playerScore=0;
-	let computerScore=0;
-
-	printFcn(introGameStr().toUpperCase() + '\n' + currentScore(playerScore,computerScore) + '\n');
-
-	for (let i=0;i<numOfRounds; i++){
-		const computerSelection = getComputerChoice();
-		let playerSelection;
-
-		do{
-			playerSelection = getPlayerChoice();
-		} while(playerSelection==-1)
-
-		let outcome = playRound(playerSelection, computerSelection); 
-      // outcome is an array: [stringDescription,associatedCode]
-		outcome[1]==winCode ? playerScore++ : outcome[1]==loseCode ? computerScore++ : null;
-
-		printFcn(currentRoundStr(i+1,numOfRounds).toUpperCase() + '\n' 
-			+ currentChoices(playerSelection,computerSelection) + '\n' 
-			+ outcome[0]+ '\n'
-			+ currentScore(playerScore,computerScore) + '\n');
-	}
-
-	printFcn(declareWinner(playerScore,computerScore).toUpperCase());
-
-	// Shown on the console how to start a new game
-	console.log(descrStartConsoleGameStr());
-}
-
-function descrStartConsoleGameStr(){
-	return `Type 'game()' or 'game(alert)' in the browser console to start a new game\n(with the alert argument, output messages are displayed in an alert box instead of the console)`;
 }
 
 function introGameStr(){
 	return `Welcome to ${choices.map(itm=>itm)} game!`;
 }
 
-function currentScore(playerScore,computerScore){
-	return `${userName}: ${playerScore} vs ${computerName}: ${computerScore}`;
+/* Helper UI functions ------------------------------------------ */
+function disableButton(btn){
+	btn.setAttribute("disabled", true);
+};
+
+function enableButton(btn){
+	btn.removeAttribute("disabled");
+};
+
+function disableArrayOfButtons(arr){
+	arr.forEach(btn => {disableButton(btn);});
+};
+
+function enableArrayOfButtons(arr){
+	arr.forEach(btn => {enableButton(btn);});
+};
+
+/* Buttons callbacks -------------------------------------------- */
+function playRound(playerSelection){
+	disableArrayOfButtons(playerChoiceBtn);
+
+	const computerSelection = getComputerChoice();
+
+	playerMoveDiv.textContent = choices[playerSelection];
+	computerMoveDiv.textContent = choices[computerSelection];
+
+	// matchOutcome is an array: [associatedCode,stringDescription]
+	let matchOutcome = roundOutcome(playerSelection, computerSelection);
+	roundOutcomeP.textContent = matchOutcome[1];
+	
+	if (matchOutcome[0]==winCode){
+		playerScore++;
+		playerScoreDiv.textContent = playerScore;
+	}else if (matchOutcome[0]==loseCode){
+		computerScore++;
+		computerScoreDiv.textContent = computerScore;
+	}
+
+	// gameOutcome is an array: [associatedCode,stringDescription]
+	let gameOutcome = declareWinner();
+	if(gameOutcome[0]){ // there is a winner for the game
+		gameOutcomeP.textContent = gameOutcome[1];
+		enableArrayOfButtons(newGameBtn);
+	}
+	else{
+		enableButton(newRoundBtn);
+	}
+};
+
+function startNewRound(){
+	disableButton(newRoundBtn);
+	enableArrayOfButtons(playerChoiceBtn);
 }
 
-function currentRoundStr(currentRound,numOfRounds){
-	return `Round ${currentRound}/${numOfRounds}`;
-}
+function startNewGame(){
+	disableArrayOfButtons(newGameBtn);
+	playerScore=0;
+	computerScore=0;
+	playerScoreDiv.textContent = playerScore;
+	computerScoreDiv.textContent = computerScore;
 
-function currentChoices(playerSelection,computerSelection){
-	return `  ${userName} chose${userName.toLowerCase()=='you'?'':'s'} ${choices[playerSelection]}, ${computerName} choses ${choices[computerSelection]}`;
-}
+	enableArrayOfButtons(playerChoiceBtn);
+};
 
-// Initially show on the console how to start a new game
-console.log(descrStartConsoleGameStr());
-
-
-/* ************************************************************** */
-
-let playerMoveDiv = document.querySelector('#playerMove');
-let playerNameDiv = document.querySelector('#playerName');
-let playerScoreDiv = document.querySelector('#playerScore');
-let computerMoveDiv = document.querySelector('#computerMove');
-let computerNameDiv = document.querySelector('#computerName');
-let computerScoreDiv = document.querySelector('#computerScore');
-let playerChoiceListDiv = document.querySelector('#playerChoiceList');
+/* Init code */
 
 
-/*Initialization*/
-playerNameDiv.textContent = userName;
-computerNameDiv.textContent = computerName;
-playerScoreDiv.textContent = 0;
-computerScoreDiv.textContent = 0;
-
-let playerChoiceBtn = [];
-for (let i=0;i<choices.length;i++){
-	playerChoiceBtn[i] = document.createElement('button');
-	playerChoiceBtn[i].type = 'button';
-	playerChoiceBtn[i].classList.add('choicesBtn',choicesLowerCase[i]);
-	playerChoiceBtn[i].textContent = choices[i];
-	playerChoiceListDiv.appendChild(playerChoiceBtn[i]);	
-}
+newGameBtn.forEach(btn => {btn.addEventListener('click',startNewGame)});
+playerChoiceBtn.forEach((btn,idx) => {btn.addEventListener('click',(event)=>{playRound(event.currentTarget.playerChoice)});});
+newRoundBtn.addEventListener('click',startNewRound);
