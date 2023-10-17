006ccc8fc7ce4012c31aa91c3e8b29859cc4b908
frarosset <37275581+frarosset@users.noreply.github.com>
2023-10-17 13:57:42 +0200

    Add player and computer animations
    Add an animation for the player and the computer.
    
    A Lottie file has been used, which allows to control which parts of
    the animations to show depending on the phase in which the game is
    on, like 'waiting for the player to choose its move' or 'showing the
    players moves' and so on.

---
+++ README.txt
@@ -0,0 +1,6 @@
+The animations used in this website can be found in the Lottie file:
+https://lottie.host/a5e54894-3981-4e58-9658-37b29d13d6ee/RFy1A9AhQP.json
+
+This file has been obtained by selecting specific frames from the Lottie
+file 'rock paper scissors' by Tom found in lottiefiles.com
+(link: https://lottiefiles.com/animations/rock-paper-scissors-Ppk2rSyyrx)
--- js/script.js
+++ js/script.js
@@ -19,6 +19,7 @@ const scoreToWin = 5;
 
 let gameInfoDiv = document.querySelector('#gameInfo');
 let playerChoiceInfoDiv = document.querySelector('#playerChoiceInfo');
+let newRoundDiv = document.querySelector('#newRound');
 let roundOutcomeDiv = document.querySelector('#roundOutcome');
 let playersInfoDiv = document.querySelector('#playersInfo');
 let gameOutcomeDiv = document.querySelector('#gameOutcome');
@@ -34,6 +35,8 @@ let newGameBtn = document.querySelector('#newGameBtn');
 let newRoundBtn = document.querySelector('#newRoundBtn');
 let roundOutcomeP = document.querySelector('#roundOutcome>p');
 let gameOutcomeP = document.querySelector('#gameOutcome>p');
+let playerMoveP = document.querySelector('#playerMove>p');
+let computerMoveP = document.querySelector('#computerMove>p');
 
 playerNameDiv.textContent = playerName;
 computerNameDiv.textContent = computerName;
@@ -49,6 +52,7 @@ for (let i=0;i<choices.length;i++){
 }
 
 removeElement(playerChoiceInfoDiv);
+removeElement(newRoundDiv);
 removeElement(roundOutcomeDiv);
 removeElement(playersInfoDiv);
 removeElement(gameOutcomeDiv);
@@ -110,10 +114,12 @@ function enableArrayOfButtons(arr){
 
 function removeElement(elem){
 	elem.classList.add('removed');
+	elem.classList.remove('hidden');
 };
 
 function hideElement(elem){
 	elem.classList.add('hidden');
+	elem.classList.remove('removed');
 };
 
 function showElement(elem){
@@ -122,66 +128,94 @@ function showElement(elem){
 
 
 /* Buttons callbacks -------------------------------------------- */
-function playRound(playerSelection){
+async function playRound(playerSelection){
 	disableArrayOfButtons(playerChoiceBtn);
 	removeElement(playerChoiceInfoDiv);
-	showElement(roundOutcomeDiv);
+
+	/*Temporary hide (instead of remove) the following two divs,
+	  to avoid moving the other elements */
+	hideElement(roundOutcomeDiv);
+	hideElement(newRoundDiv);
 
 	const computerSelection = getComputerChoice();
 
-	playerMoveDiv.textContent = choices[playerSelection];
-	computerMoveDiv.textContent = choices[computerSelection];
+	playerMoveP.textContent = choices[playerSelection];
+	computerMoveP.textContent = choices[computerSelection];
+	showElement(playerMoveP);
+	showElement(computerMoveP);
+
+	moveAnimation(playerMoveImgLottie,segmentAfterWaitBeforeMove,
+		segmentMove[playerSelection],segmentIdle[playerSelection]);
+	moveAnimation(computerMoveImgLottie,segmentAfterWaitBeforeMove,
+		segmentMove[computerSelection],segmentIdle[computerSelection]);	
 
 	// matchOutcome is an array: [associatedCode,stringDescription]
 	let matchOutcome = roundOutcome(playerSelection, computerSelection);
+
 	roundOutcomeP.textContent = matchOutcome[1];
+	showElement(roundOutcomeDiv);
 	
 	if (matchOutcome[0]==winCode){
 		playerScore++;
 		playerScoreDiv.textContent = playerScore;
-	}else if (matchOutcome[0]==loseCode){
+		await reduceAnimationSpeedAfterWaiting(computerMoveImgLottie,waitingMoveInMs);
+	} else if (matchOutcome[0]==loseCode){
 		computerScore++;
 		computerScoreDiv.textContent = computerScore;
+		await reduceAnimationSpeedAfterWaiting(playerMoveImgLottie,waitingMoveInMs);
+	} else {
+		reduceAnimationSpeedAfterWaiting(computerMoveImgLottie,waitingMoveInMs);
+		await reduceAnimationSpeedAfterWaiting(playerMoveImgLottie,waitingMoveInMs);
 	}
 
 	// gameOutcome is an array: [associatedCode,stringDescription]
 	let gameOutcome = declareWinner();
 	if(gameOutcome[0]){ // there is a winner for the game
 		gameOutcomeP.textContent = gameOutcome[1];
-		enableButton(newGameBtn);
 		showElement(gameOutcomeDiv);
 		showElement(newGameDiv);
-		hideElement(newRoundBtn);	
+		enableButton(newGameBtn);
 	}
 	else{
+		showElement(newRoundDiv);
 		enableButton(newRoundBtn);
 	}
 };
 
 function startNewRound(){
 	disableButton(newRoundBtn);
-	showElement(playerChoiceInfoDiv);
-	removeElement(roundOutcomeDiv);
-	enableArrayOfButtons(playerChoiceBtn);
+	removeElement(newRoundDiv);
+	hideElement(roundOutcomeDiv);
+	hideElement(playerMoveP);
+	hideElement(computerMoveP);	
+	showElement(playerChoiceInfoDiv);	
+	waitingAnimation(playerMoveImgLottie);
+	waitingAnimation(computerMoveImgLottie);
+	enableArrayOfButtons(playerChoiceBtn);	
 }
 
 function startNewGame(){
 	disableButton(newGameBtn);
-	removeElement(gameOutcomeDiv);
-	removeElement(roundOutcomeDiv);
 	removeElement(gameInfoDiv);
 	removeElement(newGameDiv);
-	showElement(playerChoiceInfoDiv);
-	showElement(playersInfoDiv);
-	showElement(newRoundBtn);	
-	
+	removeElement(newRoundDiv);
+	removeElement(gameOutcomeDiv);
+	hideElement(roundOutcomeDiv);
+	hideElement(playerMoveP);
+	hideElement(computerMoveP);
+
 	playerScore=0;
 	computerScore=0;
 	playerScoreDiv.textContent = playerScore;
 	computerScoreDiv.textContent = computerScore;
 
+	showElement(playerChoiceInfoDiv);
+	showElement(playersInfoDiv);
 
 
+
+	waitingAnimation(playerMoveImgLottie);
+	waitingAnimation(computerMoveImgLottie);
 	enableArrayOfButtons(playerChoiceBtn);
 };
 
@@ -189,3 +223,59 @@ function startNewGame(){
 newGameBtn.addEventListener('click',startNewGame);
 playerChoiceBtn.forEach((btn,idx) => {btn.addEventListener('click',(event)=>{playRound(event.currentTarget.playerChoice)});});
 newRoundBtn.addEventListener('click',startNewRound);
+
+/* Animated images (Lottie format) ------------------------------ */
+// https://css-tricks.com/animating-with-lottie/
+// https://stackoverflow.com/questions/51160805/bodymovin-js-not-working-animation-is-not-rendering#:~:text=Instead%20of%20bodymovin%2C%20now%20we%20have%20to%20import,And%20that%27s%20all.%20it%20should%20fix%20the%20issue.
+
+// All the images (animations) are generated from the same .json file
+let lottieData = (element) => ({
+	container: element,
+	renderer: 'svg',
+	loop: false,
+	autoplay: false,
+	path: 'https://lottie.host/a5e54894-3981-4e58-9658-37b29d13d6ee/RFy1A9AhQP.json'
+});
+
+/* The frame to use in different situations */
+let segmentWait      = [16,24];
+let segmentAfterWaitBeforeMove= [16,24];
+let segmentBeforeMove= [5,24];
+// the followings are arrays of segments to wait: see array choices to
+// see the move they refer to
+let segmentMove 	 = [[ 25, 82],[153,205],[ 89,141]]; 
+let segmentIdle 	 = [[ 54, 82],[182,205],[ 118,141]]; 
+let waitingMoveInMs = 1800;
+
+let playerMoveImg = document.querySelector('#playerMoveImg');
+let computerMoveImg = document.querySelector('#computerMoveImg');
+
+let playerMoveImgLottie = bodymovin.loadAnimation(lottieData(playerMoveImg));
+let computerMoveImgLottie = bodymovin.loadAnimation(lottieData(computerMoveImg));
+
+function waitingAnimation(animation){
+	animation.setSpeed(1.2);
+	animation.loop = true;
+	animation.playSegments(segmentWait, true);
+}
+
+// helper function to set a waiting time before executing the next line of code
+// to be used with async/await sintax
+// see https://stackoverflow.com/questions/14226803/wait-5-seconds-before-executing-next-line
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+async function reduceAnimationSpeedAfterWaiting(animation,waitingMoveInMs){
+	await delay(waitingMoveInMs);
+	animation.setSpeed(0.2);	
+}
+
+async function moveAnimation(animation,segmentBefore,segmentMove,segmentIdle){
+	animation.setSpeed(1);
+	animation.loop = true;
+	animation.playSegments([segmentBefore,segmentMove,segmentIdle], true);
+	// wait for the last animation to complete before reducing the speed
+	// for now a timeout is set
+	// TODO: instead of specifying the waiting time, wait until a complete event is fired 
+	// by the last segment of animation, see, e.g.,
+	// https://stackoverflow.com/questions/6902334/how-to-let-javascript-wait-until-certain-event-happens
+}
